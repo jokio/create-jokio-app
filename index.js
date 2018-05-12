b946a4313cdec67c0c36f2955206973099bd7c9a
Ezeki Zibzibadze <ez@jok.io>
2018-05-12 20:55:08 +0400

    renamed next to jokio

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const createNextApp = require('./lib')
+const createJokioApp = require('./lib')
 const messages = require('./lib/messages')
 
 module.exports = {
   messages: messages,
-  createNextApp: createNextApp
+  createJokioApp: createJokioApp
 }
