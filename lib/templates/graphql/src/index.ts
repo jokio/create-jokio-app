4bdbb4130a480fccb6718bafd5c28e7055cc2014
Ezeki Zibzibadze <ez@jok.io>
2018-05-18 08:10:52 +0400

    fixed tslint issues in graphql template project

--- lib/templates/graphql/src/index.ts
+++ lib/templates/graphql/src/index.ts
@@ -4,9 +4,9 @@ import localSchemas from './schemas'
 
 const port = parseInt(process.env.PORT, 10) | 3000
 const endpoint = '/'
-const logSuccessMessage = ({ port, endpoint }) => () => console.log(`GraphQL server started at http://localhost:${port}${endpoint}`)
+const logSuccessMessage = (info) => () => console.log(`GraphQL server started at http://localhost:${info.port}${info.endpoint}`)
 
 run(
     graphql({ port, localSchemas, endpoint }),
-    logSuccessMessage({ port, endpoint })
+    logSuccessMessage({ port, endpoint }),
 )
